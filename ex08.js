2c7e916c51172ae19a0770b5d9439fae0f98c682
Olivier NEUGNOT <oliviergenilac@gmail.com>
2025-03-19 11:33:49 +0100

    kpk

--- ex08.js
+++ ex08.js
@@ -1,7 +1,12 @@
 function afficherProprietes(obj) {
 	let e = "";
-	e += obj.nom + " " + obj.age + " " + obj.ville;
-	return e;
+	for (const [key, value] of Object.entries(obj)) {
+		e += `${key}: ${value}` + ", ";
+	}
+	let delet = e.split("");
+	delet.pop();
+	delet.pop();
+	return delet.join("");
 }
 
 console.log(afficherProprietes({ nom: "Alice", age: 30, ville: "Paris" })); // 'nom: Alice, age: 30, ville: Paris'
